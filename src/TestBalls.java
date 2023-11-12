d6b0817727f7bbcec2cd29952be9896d8f56dbbf
bilal84 <132393334+bilal84@users.noreply.github.com>
2023-11-12 15:13:49 +0100

    Create TestBalls.java

---
+++ src/TestBalls.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+
+public class TestBalls {
+    public static void main(String[] args) {
+        Point ball1 = new Point(0,0);
+        Point ball2 = new Point(3, 2);
+        Point ball3 = new Point(5, 0);
+
+        Point[] points = new Point[] {ball1, ball2, ball3};
+        Balls balls = new Balls(points, 3);
+
+        balls.toString();
+        balls.translate(-1, 7);
+        balls.toString();
+        balls.reInit();
+        balls.toString();
+    }
+}
